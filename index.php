9f1dd28ef4ec8ade56b2308c17982c52e18fea1e
Sparkvove <bpreweda1999@gmail.com>
2022-01-22 18:05:54 +0100

    Added Routing and basic authetication

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $path = trim($_SERVER['REQUEST_URI'], '/');
 $path = parse_url($path, PHP_URL_PATH);
 
 Routing::get('index', 'DefaultController');
-Routing::get('login', 'DefaultController');
+Routing::post('login', 'SecurityController');
 Routing::get('dashboard', 'DefaultController');
 Routing::get('search', 'DefaultController');
 Routing::get('list', 'DefaultController');
--- public/views/login.php
+++ public/views/login.php
@@ -10,10 +10,18 @@
             <img src="public/img/logo.svg">
         </div>
         <div class="login-container">
-        <form class="form-login">
+        <form class="form-login" action="login" method="POST">
+            <div class="messages">
+                <?php if (isset($messages)){
+                    foreach ($messages as $message) {
+                        echo $message;
+                    }
+                }
+                ?>
+            </div>
             <input name="email" type="text" placeholder="email@email.com">
             <input name="password" type="password" placeholder="password">
-            <button class="button-login">LOGIN</button>
+            <button type="submit" class="button-login" >LOGIN</button>
         </form>
         </div>
     </div>
