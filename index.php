901e21a8fe006b66a191b32a842a4fd206199ca4
Sparkvove <bpreweda1999@gmail.com>
2022-01-22 16:51:02 +0100

    Added last view, and added Routing

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
 
-echo 'Hi Bartosz there 👋';
+require_once 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url($path, PHP_URL_PATH);
+
+Routing::get('index', 'DefaultController');
+Routing::get('login', 'DefaultController');
+Routing::get('dashboard', 'DefaultController');
+Routing::get('search', 'DefaultController');
+Routing::get('list', 'DefaultController');
+Routing::get('book', 'DefaultController');
+Routing::run($path);
---
+++ src/controllers/DefaultController.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once 'AppController.php';
+
+class DefaultController extends AppController{
+
+
+    public function index(){
+        $this->render('login');
+    }
+
+    public function login(){
+        $this->render('login');
+    }
+
+    public function dashboard(){
+        $this->render('dashboard');
+    }
+
+    public function search(){
+        $this->render('search');
+    }
+
+    public function list(){
+        $this->render('list');
+    }
+
+    public function book(){
+        $this->render('book');
+    }
+
+
+}
