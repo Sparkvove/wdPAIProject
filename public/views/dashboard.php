51bd5431e9ed896d399c2f33c1166fee8ded5998
Sparkvove <bpreweda1999@gmail.com>
2022-01-26 13:19:11 +0100

    Added header.php so code is not duplicated

--- public/views/dashboard.php
+++ public/views/dashboard.php
@@ -10,43 +10,7 @@
 <body>
     <div class ="base-container">
 
-        <header class="main-header">
-
-             <div class="wrapper">
-
-                <div class="nav-logo">
-
-                     <img src="public/img/logo.svg">
-
-                </div>
-
-                <div class="navigation-bar">
-                    <div class="search">
-                        <i class="fas fa-search"></i>
-                        <p class="navigation-text">search</p>
-                    </div>
-
-
-                    <div class="browse">
-                        <i class="fas fa-th-large"></i>
-                        <p class="navigation-text">browse</p>
-                    </div>
-
-                    <div class="list">
-                        <i class="fas fa-book"></i>
-                        <p class="navigation-text">list</p>
-                    </div>
-
-                    <div class="settings">
-                        <i class="fas fa-cog"></i>
-                        <p class="navigation-text">settings</p>
-                    </div>
-                    
-                </div> 
-
-            </div>
-
-        </header>
+            <?php include('header.php')?>
         
        <main>
            <section class ="splitter">
